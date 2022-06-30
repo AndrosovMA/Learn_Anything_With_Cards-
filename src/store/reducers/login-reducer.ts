a1f4005a08eaa6770542fa33380fe8dd77d00fd9
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-06-30 14:37:08 +0300

    fix login-api.ts & login-reducer.ts

--- src/store/reducers/login-reducer.ts
+++ src/store/reducers/login-reducer.ts
@@ -1,44 +1,63 @@
-import {loginAPI, LoginParamsType, ResponseType} from "../../api/login-api";
+import {loginAPI, LoginParamsType, LoginResponseType} from "../../api/login-api";
 import {Dispatch} from "redux";
 
-const initialState = {}
+const initialState = {
+    isLoggedIn: false,
+    userData: {} as LoginResponseType,
+    userName: ""
+}
 //test commit
 export const loginReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case 'LOGIN/SET-IS-LOGGED-IN':
-            return {
-                ...state,
-                [action.data._id]: {
-                    _id: action.data._id,
-                    email: action.data.email,
-                    name: action.data.name,
-                    avatar: action.data.avatar,
-                    publicCardPacksCount: action.data.publicCardPacksCount, // количество колод
+        case "LOGIN/SET-IS-LOGGED-IN":
+            return {...state, isLoggedIn: action.isLoggedIn}
+        case "LOGIN/SET-USER-DATA":
+            return {...state, userData: action.userData}
+        case "LOGIN/SET-USER-NAME":
+            return {...state, userName: action.userName}
 
-                    created: action.data.created,
-                    updated: action.data.updated,
-                    isAdmin: action.data.isAdmin,
-                    verified: action.data.verified, // подтвердил ли почту
-                    rememberMe: action.data.rememberMe,
-
-                    error: action.data.error
-                } }
+        // case 'LOGIN/SET-IS-LOGGED-IN':
+        //     return {
+        //         ...state,
+        //         [action.data._id]: {
+        //             _id: action.data._id,
+        //             email: action.data.email,
+        //             name: action.data.name,
+        //             avatar: action.data.avatar,
+        //             publicCardPacksCount: action.data.publicCardPacksCount, // количество колод
+        //
+        //             created: action.data.created,
+        //             updated: action.data.updated,
+        //             isAdmin: action.data.isAdmin,
+        //             verified: action.data.verified, // подтвердил ли почту
+        //             rememberMe: action.data.rememberMe,
+        //
+        //             error: action.data.error
+        //         } }
         default:
             return state
     }
 }
 
 // actions creator
-export const setIsLoggedInAC = (data: ResponseType) =>
-    ({type: 'LOGIN/SET-IS-LOGGED-IN', data} as const)
+export const setIsLoggedInAC = (isLoggedIn: boolean) =>
+    ({type: 'LOGIN/SET-IS-LOGGED-IN', isLoggedIn} as const)
+export const setUserDataAC = (userData: LoginResponseType) =>
+    ({type: 'LOGIN/SET-USER-DATA', userData} as const)
+export const setUserNameAC = (userName: string) =>
+    ({type: 'LOGIN/SET-USER-NAME', userName} as const)
 
 // thunks
 export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType>) => {
     loginAPI.login(data)
         .then((res) => {
-            const action = setIsLoggedInAC(res.data)
-            dispatch(action)
-            console.log(res.data)
+            dispatch(setIsLoggedInAC(true))
+            dispatch(setUserDataAC(res.data))
+            dispatch(setUserNameAC(res.data.name))
+
+            // const action = setIsLoggedInAC(res.data)
+            // dispatch(action)
+            // console.log(res.data)
         })
 }
 
@@ -46,3 +65,5 @@ export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsTyp
 type InitialStateType = typeof initialState
 type ActionsType =
     | ReturnType<typeof setIsLoggedInAC>
+    | ReturnType<typeof setUserDataAC>
+    | ReturnType<typeof setUserNameAC>
