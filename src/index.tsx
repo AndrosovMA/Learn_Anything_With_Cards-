bb607f988ec86c3bce891ca016f3fe9fff265b46
Maksim Eremin <eremin1988ma21@gmail.com>
2022-07-07 16:32:34 +0300

    v6.1 - CRUD operations, need to fix

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,30 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {HashRouter} from "react-router-dom";
 import {store} from "./store/store";
+import {createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+        h1 {
+          color: grey;
+        }
+      `,
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
         <Provider store={store}>
+            <ThemeProvider theme={theme}>
             <HashRouter>
                 <App/>
             </HashRouter>
+                </ThemeProvider>
         </Provider>
 );
 
