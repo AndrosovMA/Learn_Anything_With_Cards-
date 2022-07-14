821f7fe577808fba8ecab9b38857bbfdfb073c48
Maksim Eremin <eremin1988ma21@gmail.com>
2022-07-14 17:35:21 +0300

    modal windows for CARD

---
+++ src/pages/Card/ModalDelete.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import {BsTrash} from "react-icons/bs";
+import {IconButton} from "@mui/material";
+import styled from "styled-components";
+
+export default function ModalDelete() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Wrap>
+            {/*<Button >*/}
+                <IconButton
+                    onClick={handleClickOpen}
+                    color={"error"}>
+                    <BsTrash className="btnUpdate"/>
+                </IconButton>
+            {/*</Button>*/}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending anonymous
+                        location data to Google, even when no apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Wrap>
+    );
+}
+const Wrap = styled.div`
+    
+`
